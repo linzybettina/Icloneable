c08de34fb3f41b61a4b255ba9007edd623b06f48
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-10-08 20:23:24 +0530

    Add files via upload

---
+++ Icloneable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Rock : ICloneable
+    {
+        int _weight;
+        bool _round;
+        bool _mossy;
+
+        public Rock(int weight, bool round, bool mossy)
+        {
+            this._weight = weight;
+            this._round = round;
+            this._mossy = mossy;
+        }
+
+        public object Clone()
+        {
+            return new Rock(this._weight, this._round, this._mossy);
+        }
+
+        public override string ToString()
+        {
+          return string.Format("weight = {0}, round = {1}, mossy = {2}",this._weight,this._round,this._mossy);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Rock rock1 = new Rock(10, true, false);
+            Rock rock2 = rock1.Clone() as Rock;
+
+            Console.WriteLine("1. {0}", rock1);
+            Console.WriteLine("2. {0}", rock2);
+            Console.ReadLine();
+        }
+    }
+}
